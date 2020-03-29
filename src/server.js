a6ab48d00accddba9aed3801cf24d16bbe21422d
Sporego <Hello@Filbert.me>
2020-03-29 15:36:07 -0400

    express code 2

--- src/server.js
+++ src/server.js
@@ -6,16 +6,12 @@ import * as sapper from '@sapper/server';
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-const expressServer = express().use(
-  compression({ threshold: 0 }),
-  sirv('static', { dev }),
-  sapper.middleware()
-);
-
-if (dev) {
-  expressServer.listen(PORT, err => {
+express()
+  .use(
+    compression({ threshold: 0 }),
+    sirv('static', { dev }),
+    sapper.middleware()
+  )
+  .listen(PORT, err => {
     if (err) console.log('error', err);
   });
-}
-
-export { expressServer };
