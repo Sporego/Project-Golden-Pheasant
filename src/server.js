39e985a2ec89eddb565415f320c4b9f69bb8c6c4
Sporego <Hello@Filbert.me>
2020-04-02 22:00:04 -0400

    please work Auth0

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import sirv from 'sirv';
 import express from 'express';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
+import { auth } from 'express-openid-connect';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
@@ -10,7 +11,24 @@ express()
   .use(
     compression({ threshold: 0 }),
     sirv('static', { dev }),
-    sapper.middleware()
+    auth({
+      required: false,
+      auth0Logout: true,
+      baseURL: 'http://localhost:3000',
+      issuerBaseURL: 'https://your-hosted-url.auth0.com',
+      clientID: 'your-client-id',
+      appSessionSecret: 'your-session-secret'
+    }),
+    (req, res, next) => {
+      return sapper.middleware({
+        session: () => {
+          return {
+            isAuthenticated: req.isAuthenticated(),
+            user: req.openid.user
+          };
+        }
+      })(req, res, next);
+    }
   )
   .listen(PORT, err => {
     if (err) console.log('error', err);
