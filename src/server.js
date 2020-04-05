1d7ec1c044f315975c048e518ef727f2cb350d05
Sporego <Hello@Filbert.me>
2020-04-05 16:42:53 -0400

    oops

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ const dev = NODE_ENV === 'development';
 import connectSessionSequelize from 'connect-session-sequelize';
 const SequelizeSessionStore = connectSessionSequelize(session.Store);
 
-const sequelize = new Sequelize()
+// const sequelize = new Sequelize()
 
 express()
   .use(
