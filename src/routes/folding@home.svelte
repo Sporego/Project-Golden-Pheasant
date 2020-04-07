941b8a3d1f8ceb974a4e440c50342e6211c64341
Sporego <Hello@Filbert.me>
2020-04-07 17:07:17 -0400

    complete folding @ home

--- src/routes/folding@home.svelte
+++ src/routes/folding@home.svelte
@@ -1,11 +1,19 @@
 <svelte:head>
-	<title>folding@home</title>
+  <title>folding@home</title>
 </svelte:head>
 
-<h1>China @ tirage.care</h1>
+<h1>Setup</h1>
 
 <p>
 
-
-
+  <a href="https://foldingathome.org/start-folding/">Official Download</a>
+  <br />
+  <br />
+  <a
+    href="https://github.com/FoldingAtHome/fah-issues/files/4364298/FAHViewer-091713f412-windows-x86_64.zip"
+    download>
+    Windows_ViewerFixPatch
+  </a>
+  <br />
+  ^Extract patch to root folding@home directory and overwrite existing files!"
 </p>
